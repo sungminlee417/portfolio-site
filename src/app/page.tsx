f4ecd8787007c047aadeb4f02542491c4379b3a7
Sungmin Lee <sungminlee417@gmail.com>
2025-07-16 23:21:59 -0700

    fix: change padding for responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-16 items-center p-20 w-full min-h-screen bg-gradient-to-br from-slate-800 via-slate-900 to-black text-white">
+    <div className="flex flex-col gap-16 items-center p-2 lg:p-20 w-full min-h-screen bg-gradient-to-br from-slate-800 via-slate-900 to-black text-white">
       <Header />
       <main className="w-full max-w-6xl mx-auto px-6">
         <div className="bg-white/10 backdrop-blur-md flex flex-col gap-14 rounded-2xl shadow-xl p-8 dark:bg-black/40 dark:backdrop-blur-lg">
